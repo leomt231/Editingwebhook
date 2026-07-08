8411b64d25d531551958c9da1612a5e4b8c81690
leomt231 <yapyapyaping@gmail.com>
2026-07-08 02:34:42 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -37,7 +37,6 @@ export default {
 
     const payload = {
       thread_name: name.slice(0, 100),
-      applied_tags: [1524199192275456072],
       content: "<@&1524197706925604865> new application received!",
       embeds: [embed]
     };
